43c9c8dfdde0e21ac1a083e841333d95a77cfaa9
Neha Pal <palneha1779@gmail.com>
2024-10-14 23:47:08 +0530

    Add files via upload

---
+++ g_32.py
@@ -0,0 +1,45 @@
+# Given two arrays a[] and b[] respectively of size n and m, the task is to print the count of elements in the intersection (or common elements) of the two arrays.
+
+# For this question, the intersection of two arrays can be defined as the set containing distinct common elements between the two arrays. 
+
+# Example 1:
+
+# Input:
+# n = 5, m = 3
+# a[] = {89, 24, 75, 11, 23}
+# b[] = {89, 2, 4}
+
+# Output: 1
+
+# Explanation: 
+# 89 is the only element 
+# in the intersection of two arrays.
+# Example 2:
+
+# Input:
+# n = 6, m = 5
+# a[] = {1, 2, 3, 4, 5, 6}
+# b[] = {3, 4, 5, 6, 7} 
+
+# Output: 4
+
+# Explanation: 
+# 3 4 5 and 6 are the elements 
+# in the intersection of two arrays.
+
+class Solution():
+    def intersection(self, a, b, n, m):
+        set_a = set(a)
+        set_b = set(b)
+
+        intersection = set(a) & set(b)
+
+        return len(intersection)
+    
+n = 6
+m = 5
+a = [1, 2, 3, 4, 5, 6]
+b = [3, 4, 5, 6, 7]   
+sol = Solution()
+
+print(sol.intersection(a,b,n,m))
