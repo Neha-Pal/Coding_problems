0ef4717a31ce6a36f02bd18b8c7a7cf13a67bc84
Neha Pal <palneha1779@gmail.com>
2025-01-11 00:11:55 +0530

    Add files via upload

---
+++ ArithmeticOperator.py
@@ -0,0 +1,13 @@
+    # The provided code stub reads two integers from STDIN,  and . Add code to print three lines where:
+
+    # The first line contains the sum of the two numbers.
+    # The second line contains the difference of the two numbers (first - second).
+    # The third line contains the product of the two numbers.
+
+
+a = int(input("Enter 1st number - "))
+b = int(input("Enter 2nd number - "))
+
+print("Sum - ", a+b)
+print("Difference - ", a-b)
+print("Product - ", a*b)
